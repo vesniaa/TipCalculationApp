07a95f989ccc4179eaf5f4badd75cf6ca91e59e5
Eugenia <pyssiki@yandex.ru>
2022-05-15 19:39:38 +0500

    Initial Commit

---
+++ TipCalculationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipCalculationApp
+//
+//  Created by Евгения Аникина on 15.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TipCalculationApp/TipCalculationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculationAppApp.swift
+//  TipCalculationApp
+//
+//  Created by Евгения Аникина on 15.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
